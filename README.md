d90707115f269cecb79b41b9bf3d58e64ed62a8d
ardinor <temnoyume@gmail.com>
2014-06-17 12:03:40 +1000

    Work on command line arguments

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 Frisk
 =========
 
-A simple duplicate finder written in Python 3. Stores the SHA hashes in a DB.
+A simple duplicate finder written in Python 3. Stores the SHA-1 hashes in a DB.
--- run.py
+++ run.py
@@ -1,5 +1,20 @@
+import argparse
+import os
+
 from frisk import Frisk
 
 if __name__ == "__main__":
-    frisk = Frisk("F:/Temp")
-    frisk.run()
+
+    arg_parser = argparse.ArgumentParser(description='Frisk - Duplicate detector.')
+    arg_parser.add_argument('initial_path', type=str,
+        help='The base folder from which to scan, looking for duplicates in it and subfolders.')
+
+    args = arg_parser.parse_args()
+
+    if args.initial_path:
+        if os.path.exists(args.initial_path):
+            print(args.initial_path)
+            #frisk = Frisk(args.initial_path)
+            #frisk.run()
+        else:
+            print("Invalid path.")
